0590ca8797335e4a6e84aa7ba89763dad27d057a
Smaugue <ssmaugue@gmail.com>
2025-01-07 15:03:49 +0100

    V2 Wall + Lignes simples complet

--- bot/bot.ino
+++ bot/bot.ino
@@ -1,13 +1,8 @@
 #include "ultrasonic.h"
 #include "carMotors.h"
-<<<<<<< Updated upstream
-#include"lightsensor.h"
-#define BASE_SPEED 50
-=======
 #include "lightsensor.h"
 #include <IRremote.h>       // infrared Library
 #include <FastLED.h>        // RGB Led Library
->>>>>>> Stashed changes
 
 CarMotors engine;
 LightSensor right(A0);
@@ -15,96 +10,6 @@ LightSensor center(A1);
 LightSensor left(A2);
 UltraSonic sensor;
 
-<<<<<<< Updated upstream
-void setup() {
-sensor.init();
-engine.init(BASE_SPEED);
-Serial.begin(9600);
-Serial.println("Start...");
-Serial.println();
-}
-
-void loop() {
-  Serial.println(right.readValue());
-  //engine.stop();
-  int b = checkwall();
-  int a = checkline();
-  int c = 0;
-  Serial.println(a);
-  Serial.println(b);
-  if ( a != 0 && a == b) {
-    c = b;
-  };
-  if (a==2) c=b;
-  if (b==2) c=a;
-  Serial.println(c);
-  //engine.setSpeed(BASE_SPEED);
-  if (c == 0) {
-    engine.goBackward();
-    delay(100);
-    engine.turnLeft();
-    delay(100);
-  };
-  if (c == 1) {
-    engine.goBackward();
-    delay(100);
-    engine.turnRight();
-    delay(100);
-  };
-  if (c == 2) {
-    engine.goForward();
-  };
-}
-
-int checkline() {
-  if (right.line() && left.line()!=true) {
-    engine.stop();
-    Serial.println("not ok");
-    return 0;
-  };
-  if (left.line() && right.line()!=true) {
-    engine.stop();
-    Serial.println("not ok");
-    return 1;
-  };
-  if (left.line() && right.line()) {
-    engine.stop();
-    Serial.println("not ok");
-    return 0;
-  };
-  if (left.line() && right.line() && center.line()) {
-    engine.stop();
-    Serial.println("not ok");
-    return 0;
-  };
-  return 2;
-}
-
-int checkwall() {
-  sensor.turnHead(90);
-  delay(50);
-  float dcenter = sensor.getDistance();
-  if (dcenter < 200) engine.stop();
-  sensor.turnHead(40);
-  delay(50);
-  float dleft = sensor.getDistance();
-  if (dleft < 200) engine.stop();
-  sensor.turnHead(90);
-  delay(50);
-  dcenter = sensor.getDistance();
-  if (dcenter < 200) engine.stop();
-  sensor.turnHead(140);
-  delay(50);
-  float dright = sensor.getDistance();
-  if (dcenter < 200) {
-    if (dleft > 200) return 0;
-    return 1;
-  };
-  if (dleft < 200) return 1;
-  if (dright < 200) return 0;
-  return 2;
-}
-=======
 const int RGBPin = 4;       // RGB Led pin
 const int receiverPin = 9;  // infrared signal receiving pin
 const int numLEDs = 1;
@@ -240,4 +145,3 @@ void avoidObstacle() {
   engine.turnRight();
   delay(500);
 }
->>>>>>> Stashed changes
